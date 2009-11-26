f07729cff45545df678869fec05bf9695aeaf15c
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2009-11-26 21:28:12 +0000

    Comment gardening around Zip.Compress

--- zip_lib/zip-compress-reduce.ads
+++ zip_lib/zip-compress-reduce.ads
@@ -1,3 +1,19 @@
+-- Just for fun, playing with the old Zip Reduce format...
+--
+-- Reduce combines LZ77 with a probabilistic using a Markov chain.
+-- The format has a single block for the whole file :-(.
+--
+-- NB: this compressor works in two passes, the first one
+--     serving to compute the exact Markov matrix for the whole
+--     data to be compressed. Hence, it is slow. However,
+--     we cache the last n=LZ_cache_size bytes compressed by LZ77.
+--     The result is optimal - within the constraints of that format:
+--     one block, poor encoding of compressed data and of the
+--     compression structure (the Markov matrix).
+--
+-- Author: G. de Montmollin, January 2009
+--
+
 private procedure Zip.Compress.Reduce(
   input,
   output          : Zip_Streams.Zipstream_Class;
--- zip_lib/zip-compress-shrink.adb
+++ zip_lib/zip-compress-shrink.adb
@@ -1,18 +1,3 @@
---
--- Translated by P2Ada (v. 15-Nov-2006)
--- from R. P. Byrne's Shrink.Pas (v. 1.2, 1989) in Turbo Pascal
--- and 16-bit assembler, then reworked by G. de Montmollin
---
--- Note about the LZW patent: as on 10-Dec-2007, one could read on
--- http://www.unisys.com/about__unisys/lzw :
---
---  Unisys U.S. LZW Patent No. 4,558,302 expired on June 20, 2003,
---  the counterpart patents in the United Kingdom, France, Germany
---  and Italy expired on June 18, 2004,
---  the Japanese counterpart patents expired on June 20, 2004
---  and the counterpart Canadian patent expired on July 7, 2004.
---
-
 with Ada.Unchecked_Deallocation;
 
 with Interfaces; use Interfaces;
