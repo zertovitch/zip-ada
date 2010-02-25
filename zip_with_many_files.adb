eaea80734ae8f19a51d0fa3a4a1c450029bb6b0f
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2010-02-25 20:53:56 +0000

    Zip.Create: fixed major bottlenecks around Dir_entries -> 5x faster overall for 1000 files, 356x for 100'000 ! [3:a test prog]

---
+++ zip_with_many_files.adb
@@ -0,0 +1,30 @@
+------------------------------------------------------------------------------
+--  File:            Zip_with_many_files.adb
+--  Description:     Demo/test: stuff a large number of files into a .zip file
+--  Date/version:    25-Feb-2010
+--  Author:          Gautier de Montmollin
+------------------------------------------------------------------------------
+
+with Zip_Streams;                       use Zip_Streams;
+with Zip.Create;                        use Zip.Create;
+
+procedure Zip_with_many_files is
+  stream  : aliased ZipFile_Stream;
+  archive : Zip_Create_info;
+
+  procedure Add_one_entry(to_file: String) is
+  begin
+    Zip.Create.Add_String(
+      Info              => archive,
+      Contents          => "Hello! My name is: """ & to_file & '"',
+      Name_in_archive   => to_file
+    );
+  end Add_one_entry;
+
+begin
+  Create(archive, stream'Unchecked_Access, "Large.zip");
+  for i in 1..65_535 loop
+    Add_one_entry( "Entry #" & Integer'Image(i) & ".txt");
+  end loop;
+  Finish(archive);
+end Zip_with_many_files;
