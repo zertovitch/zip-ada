5dea1b1d492e16db7745d14039d6973c94c7b656
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2012-11-05 02:00:57 +0000

    project file: moved the units using Ada.Directories_extensions at the end of the build list

--- pack_za_bin.cmd
+++ pack_za_bin.cmd
@@ -13,7 +13,7 @@ upx --ultra-brute zipada.exe
 upx --ultra-brute bunzip.exe
 :skip_upx
 
-set version=43
+set version=44
 
 zip -9 zipada%version%-bin-win32.zip comp_zip.exe find_zip.exe rezip.exe unzipada.exe zipada.exe bunzip.exe
 
