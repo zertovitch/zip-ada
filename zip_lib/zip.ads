daf63431e15ee766a66a119dc644ec933bd73e67
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2016-03-30 18:38:08 +0000

    Release Candidate v.50

--- zip_lib/zip.ads
+++ zip_lib/zip.ads
@@ -362,8 +362,8 @@ package Zip is
   -- Information about this package - e.g. for an "about" box --
   --------------------------------------------------------------
 
-  version   : constant String:= "50 preview 10b";
-  reference : constant String:= "xx-yyy-2016";
+  version   : constant String:= "50";
+  reference : constant String:= "31-Mar-2016";
   web       : constant String:= "http://unzip-ada.sf.net/";
   -- hopefully the latest version is at that URL...  ---^
 
