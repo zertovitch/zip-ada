c730509f12bee2548ec92b0d8efa5c0ba8c09b18
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2016-05-12 20:07:35 +0000

    Testing: added script for hybrid compression KZip (LZ77) + Zip.Compress.Deflate (Huffman etc.)

--- test/zakz.cmd
+++ test/zakz.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-rem Use 7Zip's LZ77 compressor, then Zip-Ada for the further stages of Deflate compression.
+rem Use KZip's LZ77 compressor, then Zip-Ada for the further stages of Deflate compression.
 
-7z a -tzip -mx=9 -mm=deflate $temp$.zip %2
+kzip $temp$.zip %2
 
 rem The following executable is obtained by setting trace = some_t in UnZip.Decompress and renaming unzipada.exe
 uza_dump_lz  -t $temp$.zip
