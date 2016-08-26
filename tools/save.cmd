0a4b769e53ab1d6ebe7d98a4f2eb07bb3295f5e9
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2016-08-26 21:10:52 +0000

    (tools) even more cleanup

--- tools/save.cmd
+++ tools/save.cmd
@@ -1,10 +1,16 @@
 call clean_za
 cd ..
+cd ..
 
-set files=z*.txt appnote.txt za*.xls *.gpr *.prj *.pra *_za.cmd make_one.cmd
-set files=%files% zip_lib/*.ad*
-set files=%files% test/*.ad* test/za_gcov.cmd test/test*.cmd test/bench*.cmd test/prof.cmd
-set files=%files% tools/*.ad* tools/rez*.cmd tools/verif.* tools/adactl*.cmd tools/save.cmd tools/clean*.cmd
-set files=%files% extras/*.ad* extras/*.a extras/*.rc extras/*.rbj extras/*.ico extras/*.pl extras/w*.cmd
+set root=zip-ada
+
+set files=%root%/z*.txt %root%/appnote.txt %root%/za*.xls %root%/*.gpr %root%/*.prj %root%/*.pra %root%/*_za.cmd %root%/make_one.cmd
+set files=%files% %root%/zip_lib/*.ad*
+set files=%files% %root%/test/*.ad* %root%/test/za_gcov.cmd %root%/test/test*.cmd %root%/test/bench*.cmd %root%/test/prof.cmd
+set files=%files% %root%/demo/*.ad* 
+set files=%files% %root%/tools/*.ad* %root%/tools/rez*.cmd %root%/tools/verif.* %root%/tools/adactl*.cmd %root%/tools/save.cmd %root%/tools/clean*.cmd
+set files=%files% %root%/extras/*.ad* %root%/extras/*.a %root%/extras/*.rc %root%/extras/*.rbj %root%/extras/*.ico %root%/extras/*.pl %root%/extras/w*.cmd
 
 zipada -ed3 za_%date%_.zip %files%
+
+cd %root%/tools
