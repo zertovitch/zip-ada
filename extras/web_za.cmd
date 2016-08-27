aba4d60d7f73637e027c33f07b6ada39274ab06a
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2016-08-27 15:35:20 +0000

    Post-release 51 cleanup

--- extras/web_za.cmd
+++ extras/web_za.cmd
@@ -2,6 +2,8 @@ cd ..
 
 call debg_za
 
-perl extras/za_html.pl -Izip_lib -Iobj_dbg -Itest -Iextras zipada unzipada rezip comp_zip find_zip demo_zip demo_unzip unzip ziptest zip.ads zip.adb lzma_dec.adb lz.adb bunzip.adb zip_with_many_files.adb -f -d -oza_html
+set mains=zipada unzipada rezip comp_zip find_zip demo_zip demo_unzip unzip ziptest zip.ads zip.adb lzma_dec lzma_enc lz.adb bunzip zip_with_many_files
+
+perl extras/za_html.pl -Izip_lib -Iobj_dbg -Itest -Iextras -Itools -Idemo %mains% -f -d -oza_html
 
 pause
--- tools/save.cmd
+++ tools/save.cmd
@@ -8,6 +8,7 @@ set files=%root%/z*.txt %root%/appnote.txt %root%/za*.xls %root%/*.gpr %root%/*.
 set files=%files% %root%/zip_lib/*.ad*
 set files=%files% %root%/test/*.ad* %root%/test/za_gcov.cmd %root%/test/test*.cmd %root%/test/bench*.cmd %root%/test/prof.cmd
 set files=%files% %root%/demo/*.ad* 
+set files=%files% %root%/doc/*.txt
 set files=%files% %root%/tools/*.ad* %root%/tools/rez*.cmd %root%/tools/verif.* %root%/tools/adactl*.cmd %root%/tools/save.cmd %root%/tools/clean*.cmd
 set files=%files% %root%/extras/*.ad* %root%/extras/*.a %root%/extras/*.rc %root%/extras/*.rbj %root%/extras/*.ico %root%/extras/*.pl %root%/extras/w*.cmd
 
