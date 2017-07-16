d4af799ec39d8c88808acfcb75b4b0a93c2fb656
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2017-07-16 14:02:37 +0000

    Comment gardening (Zip.CRC_Crypto)

--- zip_lib/zip-crc_crypto.ads
+++ zip_lib/zip-crc_crypto.ads
@@ -1,18 +1,25 @@
--- Zip.CRC_Crypto deals with pseudo-random generators for data integrity check and encryption
---
--- CRC: Cyclic redundancy check to verify archived data integrity
+--  Zip.CRC_Crypto deals with hash-like functions for data integrity check and encryption
 
 package Zip.CRC_Crypto is
 
   use Interfaces;
 
+  -------------------------------------------------------------
+  --  CRC: Cyclic Redundancy Check to verify data integrity  --
+  -------------------------------------------------------------
+  
   procedure Init( CRC: out Unsigned_32 );
-
-  function  Final( CRC: Unsigned_32 ) return Unsigned_32;
 
   procedure Update( CRC: in out Unsigned_32; InBuf: Zip.Byte_Buffer );
   pragma Inline( Update );
 
+  function  Final( CRC: Unsigned_32 ) return Unsigned_32;
+  pragma Inline( Final );
+
+  ------------------
+  --  Encryption  --
+  ------------------
+  
   type Crypto_pack is private;
   --
   type Crypto_Mode is (clear, encrypted);
@@ -22,10 +29,10 @@ package Zip.CRC_Crypto is
   procedure Init_keys(obj: in out Crypto_pack; password: String);
   --
   procedure Encode(obj: in out Crypto_pack; buf: in out Zip.Byte_Buffer);
-    pragma Inline(Encode);
+  pragma Inline(Encode);
   --
   procedure Decode(obj: in out Crypto_pack; b: in out Unsigned_8);
-    pragma Inline(Decode);
+  pragma Inline(Decode);
 
 private
   type Decrypt_keys is array( 0..2 ) of Unsigned_32;
