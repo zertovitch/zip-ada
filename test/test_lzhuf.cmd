6977275d65d5333b2e819fafc98a1427f7b74d79
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2018-09-15 22:12:37 +0000

    Added testing scripts for non-Zip-archive encoders/decoders

---
+++ test/test_lzhuf.cmd
@@ -0,0 +1,15 @@
+if "%1"=="" goto fin
+
+rem  Custom temp dir
+set temp0=e:\temp
+
+rem  Compress
+..\lz e %1 %temp0%\lzhuf.dat
+rem  Decompress
+..\lz d %temp0%\lzhuf.dat %temp0%\lzhuf_dec.dat 
+
+rem  Test against original
+fc /b %temp0%\lzhuf_dec.dat %1
+
+:fin
+pause
---
+++ test/test_lzma.cmd
@@ -0,0 +1,15 @@
+if "%1"=="" goto fin
+
+rem  Custom temp dir
+set temp0=e:\temp
+
+rem  Compress
+..\lzma_enc %1 %temp0%\lzma_dat
+rem  Decompress
+..\lzma_dec %temp0%\lzma_dat.lzma %temp0%\lzma_dec.dat 
+
+rem  Test against original
+fc /b %temp0%\lzma_dec.dat %1
+
+:fin
+pause
