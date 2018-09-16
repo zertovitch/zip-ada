991a950491b75a7d3f4836a7f01d36eae1d80a1a
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2018-09-16 18:48:34 +0000

    test_non_zip ('plain' implemented)

--- test/test_non_zip.adb
+++ test/test_non_zip.adb
@@ -4,15 +4,41 @@
 --
 --  Comparison nz_0.zip, nz_lzhuf.zip, nz_lzma.zip: should be identical.
 
-
 with Comp_Zip_Prc, LZH, LZMA.Encoding, LZMA.Decoding;
 
-with Ada.Command_Line;  use Ada.Command_Line;
-with Ada.Text_IO;       use Ada.Text_IO;
+with Zip_Streams;                       use Zip_Streams;
+with Zip.Create;                        use Zip.Create;
+
+with Ada.Command_Line;                  use Ada.Command_Line;
+with Ada.Text_IO;                       use Ada.Text_IO;
 
 procedure Test_non_zip is
+
+  type Raw_scheme is (plain, lzhuf, lzma);
+
+  nz : array (Raw_scheme) of aliased File_Zipstream;
+  info : array (Raw_scheme) of Zip_Create_info;
+
+  procedure Test_LZHuf (fn: String) is
+  begin
+    null;
+  end Test_LZHuf;
+
 begin
+  for r in Raw_scheme loop
+    Create (info (r), nz (r)'Unchecked_Access, "$nz" & Raw_scheme'Image (r) & ".zip");
+  end loop;
+  --
   for i in 1 .. Argument_Count loop
-    Put_Line("Testing raw compression on: " & Argument(i));
+    Put_Line ("Testing raw compression on: " & Argument(i));
+    --  Plain :
+    Add_File (info (plain), Argument (i));
+    --  LZHuf :
+    Test_LZHuf (Argument (i));
+    --  LZMA  :
+  end loop;
+  --
+  for r in Raw_scheme loop
+    Finish (info (r));
   end loop;
 end Test_non_zip;
