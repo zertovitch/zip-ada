e2cca128c432e8e0189437488547973507fafc6e
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2018-10-02 14:49:05 +0000

    Comment gardening in test & demo for Extract's file_system_routines parameter

--- demo/demo_unzip.adb
+++ demo/demo_unzip.adb
@@ -8,11 +8,19 @@ procedure Demo_UnZip is
   c: Character;
 begin
   --  Make sure we have the Zip file for the demo...
+  --
   Demo_zip;
+  --
+  --  Extract to a file.
+  --    You can customize this with Create_Path (creation of missing paths)
+  --    and Compose_File_Name (add a custom prefix directory for instance)
+  --    via the file_system_routines parameter. See tools/unzipada.adb for
+  --    a complete example.
   --
   Extract("mini_zip.zip", "demo/demo_unzip.adb", "demo_unzip_$unzipped$.adb");
   --
-  --  Testing UnZip.Streams: we just output the contents of an entry to standard output
+  --  Testing UnZip.Streams: we just output the contents of an entry
+  --  to standard output.
   --
   Open(f, "mini_zip.zip", "demo/demo_unzip.adb");
   s:= Stream(f);
--- test/test_extract.adb
+++ test/test_extract.adb
@@ -2,6 +2,12 @@ with UnZip; use UnZip;
 
 procedure Test_Extract is
 begin
+  --  Extract to a file.
+  --    You can customize this with Create_Path (creation of missing paths)
+  --    and Compose_File_Name (add a custom prefix directory for instance)
+  --    via the file_system_routines parameter. See tools/unzipada.adb for
+  --    a complete example.
+  --
   Extract("aaa.zip");                             -- ok
   Extract("aaa.zip", "file2.txt");                -- ok
   Extract("aaa.zip", "file2.txt", "file2_b.txt"); -- ok
