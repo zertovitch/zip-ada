d5c3e9a95265d9bdbc06b4e65ef988acf8853f42
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2018-10-08 06:05:42 +0000

    test/zip_with_many_files.adb creates several archives

--- test/zip_with_many_files.adb
+++ test/zip_with_many_files.adb
@@ -14,33 +14,50 @@ with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
 with Ada.Strings.Fixed;                 use Ada.Strings.Fixed;
 
 procedure Zip_with_many_files is
-  stream  : aliased File_Zipstream;
-  archive : Zip_Create_info;
 
-  procedure Add_one_entry(file_name: String; rep: Natural) is
+  procedure Create_with_many (n: Positive) is
+    stream  : aliased File_Zipstream;
+    archive : Zip_Create_info;
+
+    procedure Add_one_entry(file_name: String; rep: Natural) is
+    begin
+      Zip.Create.Add_String(
+        Info              => archive,
+        Contents          => "..." & rep * ("Hello! My name is: """ & file_name & '"' & ASCII.LF),
+        Name_in_archive   => file_name
+      );
+    end Add_one_entry;
+
+    function Leading_zeros (i, zeros: Integer) return String is
+      pad: constant Integer := 10 ** zeros;
+      str: String (1..zeros+2);
+    begin
+      Put (str, i + pad);
+      return str (3 .. str'Last);
+    end Leading_zeros;
+
+    n_img : constant String := n'Image;
+
   begin
-    Zip.Create.Add_String(
-      Info              => archive,
-      Contents          => "..." & rep * ("Hello! My name is: """ & file_name & '"' & ASCII.LF),
-      Name_in_archive   => file_name
+    Create(
+      archive,
+      stream'Unchecked_Access,
+      n_img (n_img'First + 1 .. n_img'Last) & ".zip",
+      Zip.Compress.Deflate_1
     );
-  end Add_one_entry;
-
-  function Leading_zeros (i, zeros: Integer) return String is
-    pad: constant Integer := 10 ** zeros;
-    str: String (1..zeros+2);
-  begin
-    Put (str, i + pad);
-    return str (3 .. str'Last);
-  end Leading_zeros;
-
+    for i in 1 .. n loop
+      Add_one_entry(
+        "Entry #" & Leading_zeros (i, 5) & ".txt",
+        Integer'Max(0, i / 100 - 10)  --  Obtain a certain number of incompressible entries.
+      );
+    end loop;
+    Finish(archive);
+  end Create_with_many;
 begin
-  Create(archive, stream'Unchecked_Access, "many.zip", Zip.Compress.Deflate_1);
-  for i in 1 .. 65_535 loop
-    Add_one_entry(
-      "Entry #" & Leading_zeros (i, 5) & ".txt",
-      Integer'Max(0, i / 100 - 10)  --  Obtain a certain number of incompressible entries.
-    );
-  end loop;
-  Finish(archive);
+  Create_with_many (2 ** 12);
+  Create_with_many (2 ** 13);
+  Create_with_many (2 ** 14);
+  Create_with_many (2 ** 15);
+  Create_with_many (2 ** 16 - 1);
+  Create_with_many (2 ** 16);  --  Should raise Zip_Capacity_Exceeded in Zip_32 mode.
 end Zip_with_many_files;
