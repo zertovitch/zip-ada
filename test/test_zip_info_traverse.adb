675ac0f88ff2bb701324575339644518041b684d
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2018-10-09 10:09:36 +0000

    Added another test, test_zip_info_traverse.adb

---
+++ test/test_zip_info_traverse.adb
@@ -0,0 +1,14 @@
+with Zip, Ada.Command_Line, Ada.Text_IO;
+
+procedure Test_Zip_Info_Traverse is
+  use Zip, Ada.Command_Line, Ada.Text_IO;
+  zi: Zip_info;
+  procedure Action (name: String) is
+  begin
+    Put_Line (name);
+  end Action;
+  procedure Listing is new Traverse (Action);
+begin
+  Load (zi, Argument (1), duplicate_names => admit_duplicates);
+  Listing (zi);
+end Test_Zip_Info_Traverse;
