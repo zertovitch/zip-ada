1c4a9bdaef9303f8664ad920c2ba1b81f44366cb
gdemont <gdemont@e6c10d1f-90c4-4b5f-9d68-0f1cda5ddf12>
2019-07-06 16:52:20 +0000

    Improved scripts for saving sources and doc into a Zip file

--- tools/save.cmd
+++ tools/save.cmd
@@ -28,16 +28,21 @@ set ver=%1
 if "%1"=="" echo *** No revision number given, putting XXX
 if "%1"=="" set ver=XXX
 
-set root=zip-ada
+set root=za
 
-set files=%root%/z*.txt %root%/doc/appnote.txt %root%/doc/za*.xls %root%/*.gpr %root%/*.prj %root%/*.pra %root%/*_za.cmd %root%/make_one.cmd
+set files=%root%/z*.txt %root%/*.gpr %root%/*.prj %root%/*.pra
+rem Ada sources
 set files=%files% %root%/zip_lib/*.ad*
-set files=%files% %root%/test/*.ad* %root%/test/za_gcov.cmd %root%/test/test*.cmd %root%/test/bench*.cmd %root%/test/prof.cmd
+set files=%files% %root%/test/*.ad*
 set files=%files% %root%/demo/*.ad* 
 set files=%files% %root%/trained/*.ad* %root%/trained/*.gpr
-set files=%files% %root%/doc/*.txt
+rem Documentation
+set files=%files% %root%/doc/*.txt %root%/doc/za*.xls
 set files=%files% %root%/tools/*.ad* %root%/tools/rez*.cmd %root%/tools/verif.* %root%/tools/adactl*.cmd %root%/tools/save.cmd %root%/tools/clean*.cmd
 set files=%files% %root%/extras/*.ad* %root%/extras/*.a %root%/extras/*.rc %root%/extras/*.rbj %root%/extras/*.ico %root%/extras/*.pl %root%/extras/w*.cmd
+rem Scripts
+set files=%files% %root%/*_za.cmd %root%/make_one.cmd %root%/*.sh
+set files=%files% %root%/test/za_gcov.cmd %root%/test/test*.cmd %root%/test/bench*.cmd %root%/test/prof.cmd %root%/test/*.sh
 
 zipada -ep2 za_%nice_date%_%ver%.zip %files%
 
--- tools/save.sh
+++ tools/save.sh
@@ -8,11 +8,16 @@ cd ..
 cd ..
 
 files="$root/*.gpr $root/*.pra"
+# Ada sources
 files="$files $root/zip_lib/*.ad*"
 files="$files $root/demo/*.ad* $root/extras/*.ad*"
 files="$files $root/test/*.ad* $root/tools/*.ad*"
 files="$files $root/trained/*.ad* $root/trained/*.gpr"
+# Documentation
 files="$files $root/doc/*.txt $root/doc/*.xls"
+# Scripts
+files="$files $root/*.cmd $root/*.sh"
+files="$files $root/tools/*.cmd $root/tools/*.sh"
 
 # echo $nice_date
 # echo $files
