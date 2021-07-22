9e46974df0f2dbb00b2281ad90d4c4b6802f04ea
zertovitch <gdemont@hotmail.com>
2021-07-22 21:05:43 +0200

    (scripts) Benchmark script produces separate log files per test set

--- test/bench.cmd
+++ test/bench.cmd
@@ -84,7 +84,7 @@ dir /OS- bench_%1_*.*
 rem Wrapping it all - update archive with all archives 
 rem Funnily, upon zipping bench_matrix_*.zip, level 1-8 stores everything, but level 9 deflates to 26% !
 
-zip -9 all_bench_%1.zip bench_%1_*.zip
+zip -9 all_bench_%1.zip bench_%1_*.zip bench_%1_*.lzma bench_%1_*.7z
 echo.
 echo ***************************************************************
 echo ********** Test the archives, bench_%1_*.zip
@@ -93,14 +93,17 @@ echo.
 7z t bench_%1_*.zip | tail
 echo ***************************************************************
 dir /OS- bench_%1_*.zip
-echo.>>benchs.log
-echo -- [%1] -- >>benchs.log
+echo.>>bench_%1.log
+echo -- [%1] [%date% %time%] -- >>bench_%1.log
 
 rem  Delete individual method-related archives
-if exist bench_%1_*.zip del bench_%1_*.zip
+if exist bench_%1_*.zip  del bench_%1_*.zip
+if exist bench_%1_*.lzma del bench_%1_*.lzma
+if exist bench_%1_*.7z   del bench_%1_*.7z
+if exist bench_%1.tar    del bench_%1.tar
 
 unzip -v all_bench_%1.zip | find ".zip" | find "%%" | sort
-unzip -v all_bench_%1.zip | find ".zip" | find "%%" | sort>>benchs.log
+unzip -v all_bench_%1.zip | find ".zip" | find "%%" | sort>>bench_%1.log
 
 goto fin
 
