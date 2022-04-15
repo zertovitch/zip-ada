ec1c26ac7cd24a550927f2c44caa5304144c1f23
zertovitch <gdemont@hotmail.com>
2022-04-15 17:41:16 +0200

    (extras) Set_Modification_Time_GNAT, simplification: using procedure Set_File_Last_Modify_Time_Stamp which is visible in GNAT.OS_Lib

--- extras/set_modification_time_gnat.adb
+++ extras/set_modification_time_gnat.adb
@@ -1,26 +1,21 @@
-with Ada.Calendar,
-     Ada.Directories;
+--  Reverse operation to the function Ada.Directories.Modification_Time.
 
-with System;
+with Ada.Calendar;
 with GNAT.OS_Lib;
 
 procedure Set_Modification_Time_GNAT
   (Name : in String;
    To   : in Ada.Calendar.Time)
 is
-  use Ada.Directories, Ada.Calendar, GNAT.OS_Lib;
-  t : constant OS_Time :=
+  use Ada.Calendar, GNAT.OS_Lib;
+
+  GNAT_Time : constant OS_Time :=
     GM_Time_Of (Year   => Year (To),
                 Month  => Month (To),
                 Day    => Day (To),
                 Hour   =>  Integer (Seconds (To)) / 3600,
                 Minute => (Integer (Seconds (To)) / 60) mod 60,
                 Second => Integer (Seconds (To)) mod 60);
-  procedure C_Set_Modification_Time (N : System.Address; T : OS_Time);
-  pragma Import (C, C_Set_Modification_Time, "__gnat_set_file_time_name");
-  F_Name : constant String := Full_Name (Name);
-  C_Name : aliased String (1 .. F_Name'Length + 1);
 begin
-  C_Name := F_Name & ASCII.NUL;
-  C_Set_Modification_Time (C_Name'Address, t);
+  Set_File_Last_Modify_Time_Stamp (Name, GNAT_Time);
 end Set_Modification_Time_GNAT;
