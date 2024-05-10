93b3410e3346f80fad6e2d29a26bb7543cf4ab19
unknown <gdemont@hotmail.com>
2024-05-10 11:06:55 +0200

    (extras) Updated scrpit to generate html files from Zip-Ada's Ada files

--- extras/web_za.cmd
+++ extras/web_za.cmd
@@ -1,9 +1,11 @@
 cd ..
 
-call debg_za
+gprbuild -Pzipada      -XZip_Build_Mode=Debug
+gprbuild -Pzipada_test -XZip_Build_Mode=Debug
 
 set mains=zipada unzipada rezip comp_zip find_zip demo_zip demo_unzip unzip ziptest zip.ads zip.adb lzma_dec lzma_enc lzhuf.adb bunzip zip_with_many_files
 
-perl extras/za_html.pl -Izip_lib -Iobj_dbg -Itest -Iextras -Itools -Idemo %mains% -f -d -oza_html
+perl extras/za_html.pl -Izip_lib -Iobj_dbg -Iobj_dbg/test -Itest -Iextras -Itools -Idemo %mains% -f -d -oza_html
 
+cd extras
 pause
