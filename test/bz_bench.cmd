254abd8be8c1580f4898e915d0018dc7be1e1342
unknown <gdemont@hotmail.com>
2024-11-20 20:00:09 +0100

    (tests) Updated bzip2 benchmark script

--- test/bz_bench.cmd
+++ test/bz_bench.cmd
@@ -1,17 +1,29 @@
 @echo off
 
-for /L %%i in (1,1,9) do (
-  bzip2 -k -%%i %1
-  if exist %1_%%i.bz2 del %1_%%i.bz2
-  mv %1.bz2 %1_%%i.bz2
-  bzip2_dec %1_%%i.bz2 %1_%%i.out
-  )
+REM for /L %%i in (1,1,9) do (
+  REM bzip2 -k -%%i %1
+  REM if exist %1_%%i.bz2 del %1_%%i.bz2
+  REM mv %1.bz2 %1_%%i.bz2
+  REM bzip2_dec %1_%%i.bz2 %1_%%i.out
+  REM )
   
-for /L %%i in (1,1,4) do (
-  bzip2_enc %1 %1_%%i_ada -%%i
-  bzip2_dec    %1_%%i_ada.bz2 %1_%%i_ada.out
-  )
+REM for /L %%i in (1,1,4) do (
+  REM bzip2_enc %1 %1_%%i_ada -%%i
+  REM bzip2_dec    %1_%%i_ada.bz2 %1_%%i_ada.out
+  REM )
   
-for /L %%i in (1,1,9) do comp /M %1 %1_%%i.out
-for /L %%i in (1,1,4) do comp /M %1 %1_%%i_ada.out
+REM for /L %%i in (1,1,9) do comp /M %1 %1_%%i.out
+REM for /L %%i in (1,1,4) do comp /M %1 %1_%%i_ada.out
 
+del $_bb_*max.zip
+
+zip -9 -Z bzip2                     $_bb_iz_bzip2_max %1
+7z a -tzip -mm=BZip2:d=900k:pass=99 $_bb_7z_bzip2_max %1
+..\zipada -eb3                      $_bb_za_bzip2_max %1
+
+7z a -tzip -mm=LZMA -mx9            $_bb_7z_lzma_max  %1
+zipada -el3                         $_bb_za_lzma_max  %1
+
+7z t $_bb_*max.zip
+
+dir $_bb_*max.zip
