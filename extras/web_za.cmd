57522376b1eccc8cb8cfe4c80330508cd41442a7
unknown <gdemont@hotmail.com>
2025-02-13 19:32:21 +0100

    (extras) Updated script for source browser generator

--- extras/web_za.cmd
+++ extras/web_za.cmd
@@ -5,7 +5,7 @@ gprbuild -Pzipada_test -XZip_Build_Mode=Debug
 
 set mains=zipada unzipada rezip comp_zip find_zip demo_zip demo_unzip unzip ziptest zip.ads zip.adb lzma_dec lzma_enc lzhuf.adb bunzip zip_with_many_files
 
-perl extras/za_html.pl -Izip_lib -Iobj_dbg -Iobj_dbg/test -Itest -Iextras -Itools -Idemo %mains% -f -d -oza_html
+perl extras/za_html.pl -Izip_lib -Iobj/dbg -Iobj/dbg/test -Itest -Iextras -Itools -Idemo %mains% -f -d -oza_html
 
 cd extras
 pause
