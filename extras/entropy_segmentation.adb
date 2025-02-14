0fb7b82d042d028b65656654aaeccbd8aea30f37
unknown <gdemont@hotmail.com>
2025-02-14 19:13:10 +0100

    (extras) Improved entropy_segmentation.adb

--- extras/entropy_segmentation.adb
+++ extras/entropy_segmentation.adb
@@ -1,3 +1,5 @@
+--  Experimental / research program.
+
 with Ada.Containers.Vectors,
      Ada.Command_Line,
      Ada.Directories,
@@ -25,9 +27,9 @@ procedure Entropy_Segmentation is
     type Real is digits 15;
     package REF is new Ada.Numerics.Generic_Elementary_Functions (Real);
     use REF;
-    discrepancy_threshold : constant := 2.0;      --  Discrepancy detection threshold.
-    index_threshold       : constant := 200_000;  --  Do segmentation only above a certain distance.
-    window_size           : constant := 50_000;   --  Sliding window size.
+    discrepancy_threshold : constant := 1.9;      --  Discrepancy detection threshold.
+    index_threshold       : constant := 20_000;   --  Do segmentation only above a certain distance.
+    window_size           : constant := 10_000;   --  Sliding window size.
     reporting_period : constant := 1000;
     inv_window_size : constant Real := 1.0 / Real (window_size);
     len : constant Index := Index (buffer.Length);
@@ -43,51 +45,53 @@ procedure Entropy_Segmentation is
     bt : Byte;
   begin
     seg.Clear;
-    for i in 1 .. len loop
-      --  Fill the sliding window.
-      bt := buffer (i);
-      freq (bt) := freq (bt) + 1;
-      if i = window_size then
-        --  Compute initial entropy value.
-        for b in Byte loop
-          p := Prob (b);
-          if p > 0.0 then
-            elem (b) := Entropy_Function (p);
-            entropy := entropy + elem (b);
-          end if;
-        end loop;
-        entropy_mark := entropy;
-      elsif i > window_size then
-        --  Adjust entropy for new value coming in.
-        entropy := entropy - elem (bt);
-        p := Prob (bt);
-        --  Note: count (bt) is positive, px too.
-        elem (bt) := Entropy_Function (p);
-        entropy := entropy + elem (bt);
-        --  Adjust entropy for old value disappearing.
-        bt := buffer (i - window_size);
-        entropy := entropy - elem (bt);
-        freq (bt) := freq (bt) - 1;
-        p := Prob (bt);
-        if p > 0.0 then
+    if len > window_size + index_threshold then
+      for i in 1 .. len loop
+        --  Fill the sliding window.
+        bt := buffer (i);
+        freq (bt) := freq (bt) + 1;
+        if i = window_size then
+          --  Compute initial entropy value.
+          for b in Byte loop
+            p := Prob (b);
+            if p > 0.0 then
+              elem (b) := Entropy_Function (p);
+              entropy := entropy + elem (b);
+            end if;
+          end loop;
+          entropy_mark := entropy;
+        elsif i > window_size then
+          --  Adjust entropy for new value coming in.
+          entropy := entropy - elem (bt);
+          p := Prob (bt);
+          --  Note: count (bt) is positive, px too.
           elem (bt) := Entropy_Function (p);
           entropy := entropy + elem (bt);
-        else
-          elem (bt) := 0.0;
-        end if;
-        if abs (entropy - entropy_mark) > discrepancy_threshold then
-          seg_point := i - window_size;
-          if seg_point - index_mark > index_threshold then
-            seg.Append (seg_point);
-            index_mark := seg_point;
-            entropy_mark := entropy;
+          --  Adjust entropy for old value disappearing.
+          bt := buffer (i - window_size);
+          entropy := entropy - elem (bt);
+          freq (bt) := freq (bt) - 1;
+          p := Prob (bt);
+          if p > 0.0 then
+            elem (bt) := Entropy_Function (p);
+            entropy := entropy + elem (bt);
+          else
+            elem (bt) := 0.0;
+          end if;
+          if abs (entropy - entropy_mark) > discrepancy_threshold then
+            seg_point := i - window_size;
+            if seg_point - index_mark > index_threshold then
+              seg.Append (seg_point);
+              index_mark := seg_point;
+              entropy_mark := entropy;
+            end if;
+          end if;
+          if i mod reporting_period = len mod reporting_period then
+            Ada.Text_IO.Put_Line (f_csv, entropy'Image);
           end if;
         end if;
-        if i mod reporting_period = len mod reporting_period then
-          Ada.Text_IO.Put_Line (f_csv, entropy'Image);
-        end if;
-      end if;
-    end loop;
+      end loop;
+    end if;
     seg.Append (len);
   end Segment;
 
@@ -126,7 +130,7 @@ begin
     buffer.Append (Read_Byte);
   end loop;
 
-  Create (f_csv, Out_File, "dump_" & Simple_Name (Name (f_in)) & ".csv");
+  Create (f_csv, Out_File, "entropy_" & Simple_Name (Name (f_in)) & ".csv");
   Put_Line (f_csv, "Entropy");
 
   Segment (seg);
